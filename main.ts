3261bfd9edf486db5597ea3e5889eb2580469fda
mike connor <mike@rocketnumber9.org>
2020-02-01 09:40:19 -0600

    typescript
    caught error.  Need to cast nodeList to Array

--- main.ts
+++ main.ts
@@ -1,19 +1,19 @@
 (function () {
-    var icons = [].slice.call(document.querySelectorAll("div.icon"));
-    var activeIcon;
-    icons.forEach(function (icon) {
+
+    const icons = [].slice.call(document.querySelectorAll("div.icon"));
+    let activeIcon: HTMLElement;
+    icons.forEach((icon) => {
         icon.addEventListener('click', function () {
             if (!activeIcon) {
                 this.classList.add('spin');
-            }
-            else if (activeIcon === this) {
+            } else if (activeIcon === this) {
                 this.classList.toggle('spin');
-            }
-            else {
+            } else {
                 activeIcon.classList.remove('spin');
                 this.classList.add('spin');
             }
             activeIcon = this;
         });
     });
-})();
+
+})()
